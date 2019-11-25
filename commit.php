d3a6881c102bd9e9aaab1d9c89487e5bb0a8f2cb
YxMingY <526817087@qq.com>
2019-11-26 00:03:13 +0800

    提交页面

--- commit.php
+++ commit.php
@@ -4,14 +4,6 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>家庭作业提交系统</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<style type="text/css">
-		#hw-img{
-			height: 1000px;
-			width: 600px;
-			border: 5px solid black;
-			margin: 0 auto;
-		}
-	</style>
 	<script type="text/javascript">
 		function commit(event){
 			var file = event.target.files[0];
@@ -30,10 +22,10 @@
 		//var_dump(file_get_contents($_FILES["img"]["tmp_name"]));
 		if(!isset($file) || $file["error"] != 0){
 			echo
-			'<form method="post" enctype="multipart/form-data">
-			<p>名字:<input type="text" name="name"></p>
-		    <p><input type="file" name="img" onchange="commit(event)"></p>
-		    <input type="submit" value="submit">
+			'<form id="upload-form" method="post" enctype="multipart/form-data">
+			名字:<input type="text" name="name">
+		    <input type="file" name="img" onchange="commit(event)">
+		    <input type="submit" value="提交">
 		    </form>
 		    ';
 		}else{
